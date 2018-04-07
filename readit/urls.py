bce1629c7d38c3d8603f085a165923c400d5fa50
Jason Xie <jason@Jasons-MacBook-Pro.local>
2018-04-08 06:08:11 +0800

    Create our first URL and view

--- readit/urls.py
+++ readit/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from books.views import list_books
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', list_books, name='books')
 ]
