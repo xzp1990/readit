00ab725bb59aadd4be68fe45f886e975c16d34ab
Jason Xie <jason@Jasons-MacBook-Pro.local>
2018-04-09 12:33:27 +0800

    Process ReviewForm

--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Count
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import View, DetailView
 from .models import Author, Book
 from .forms import ReviewForm
@@ -64,7 +64,19 @@ def review_book(request, pk):
     :return:
     """
     book = get_object_or_404(Book, pk=pk)
-    form = ReviewForm
+
+    if request.method == 'POST':
+        # Process our form
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            book.is_favourite = form.cleaned_data['is_favourite']
+            book.review = form.cleaned_data['review']
+            book.save()
+
+            return redirect('review-books')
+    else:
+        form = ReviewForm
 
     context = {
         'book': book,
