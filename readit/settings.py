16bf40aef1009a880a0d2da799a07970c26c2df4
Jason Xie <jason@Jasons-MacBook-Pro.local>
2018-04-12 15:01:04 +0800

    Update settings and requirements to support local development?

--- readit/settings.py
+++ readit/settings.py
@@ -92,7 +92,17 @@ if DJANGO_MODE == 'local':
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
+elif DJANGO_MODE == 'staging':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+            'PORT': os.getenv('DB_PORT', '5432'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
